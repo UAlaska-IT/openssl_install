2ac9b8b1b560f2c6c744240868244802523ede0e
Caleb Severn <calsev@gmail.com>
2019-06-27 15:36:47 -0800

    Added library files

---
+++ libraries/openssl_install_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# This module implements helpers that are used for resources
+module OpenSslInstall
+  # This module implements helpers that are used for resources
+  module Helper
+  end
+end

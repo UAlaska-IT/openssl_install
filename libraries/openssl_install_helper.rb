e1b6a70587a93916e65a1d068df60791a1654c1d
Caleb Severn <calsev@gmail.com>
2019-07-02 11:53:25 -0800

    Reorganized helpers

--- libraries/openssl_install_helper.rb
+++ libraries/openssl_install_helper.rb
@@ -4,10 +4,6 @@
 module OpenSslInstall
   # This module implements helpers that are used for resources
   module Helper
-    def openssl_download_url(version)
-      return "https://www.openssl.org/source/openssl-#{version}.tar.gz"
-    end
-
     def path_to_download_directory(given_directory)
       return given_directory if given_directory
 
@@ -20,6 +16,18 @@ module OpenSslInstall
       return File.join(directory, "openssl-#{version}")
     end
 
+    def download_url(version)
+      return "https://www.openssl.org/source/openssl-#{version}.tar.gz"
+    end
+
+    def download_archive(given_download_dir, version)
+      download_file = path_to_download_file(given_download_dir, version)
+      remote_file download_file do
+        source download_url(version)
+      end
+      return download_file
+    end
+
     def path_to_build_directory(given_directory, version)
       return given_directory if given_directory
 
@@ -27,38 +35,20 @@ module OpenSslInstall
       return "/var/chef/cache/openssl-#{version}"
     end
 
+    def extract_archive(new_resource, build_directory, version)
+      download_file = download_archive(new_resource.download_dir, version)
+
+      poise_archive download_file do
+        destination build_directory
+        user new_resource.owner
+        group new_resource.group
+      end
+    end
+
     def openssl_install_directory(given_directory, version)
       return given_directory if given_directory
 
       return "/opt/openssl/#{version}"
-    end
-
-    def openssl_inc_directory(install_directory)
-      return File.join(install_directory, 'include')
-    end
-
-    def openssl_lib_directory(install_directory)
-      return File.join(install_directory, 'lib')
-    end
-
-    def download_archive(given_download_dir, version)
-      download_file = path_to_download_file(given_download_dir, version)
-
-      remote_file download_file do
-        source openssl_download_url(year, version)
-      end
-
-      return download_file
-    end
-
-    def extract_archive(given_download_dir, build_directory, version, user, group)
-      download_file = download_archive(given_download_dir, version)
-
-      poise_archive download_file do
-        destination build_directory
-        user user
-        group group
-      end
     end
 
     def create_config_code(install_directory, strict_security)
@@ -69,7 +59,7 @@ module OpenSslInstall
       return code
     end
 
-    def configure_openssl(build_directory, install_directory, strict_security)
+    def configure_build(build_directory, install_directory, strict_security)
       code = create_config_code(install_directory, strict_security)
       bash 'Configure OpenSSL' do
         code code
@@ -78,7 +68,7 @@ module OpenSslInstall
       end
     end
 
-    def compile_openssl(build_directory, install_directory)
+    def compile_and_install(build_directory, install_directory)
       bash 'Compile OpenSSL' do
         code 'make && make install'
         cwd build_directory
@@ -86,13 +76,25 @@ module OpenSslInstall
       end
     end
 
+    def build_binary(build_directory, given_install_directory, strict_security)
+      install_directory = openssl_install_directory(given_install_directory, version)
+      configure_build(build_directory, install_directory, strict_security)
+      compile_and_install(build_directory, install_directory)
+    end
+
     def create_openssl_install(new_resource)
       version = new_resource.version
       build_directory = path_to_build_directory(new_resource.build_directory, version)
-      extract_archive(new_resource.download_directory, build_directory, version, new_resource.user, new_resource.group)
-      install_directory = openssl_install_directory(new_resource.install_directory, version)
-      configure_openssl(build_directory, install_directory, new_resource.strict_security)
-      compile_openssl(build_directory, install_directory)
+      extract_archive(new_resource, build_directory, version)
+      build_binary(build_directory, new_resource.install_directory, new_resource.strict_security)
+    end
+
+    def openssl_inc_directory(install_directory)
+      return File.join(install_directory, 'include')
+    end
+
+    def openssl_lib_directory(install_directory)
+      return File.join(install_directory, 'lib')
     end
   end
 end

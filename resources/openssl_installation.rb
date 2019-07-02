217c154684d207511cc606c736564f691b16fade
Caleb Severn <calsev@gmail.com>
2019-07-02 11:10:40 -0800

    Updated resource interface

--- resources/openssl_installation.rb
+++ resources/openssl_installation.rb
@@ -5,10 +5,13 @@ provides :openssl_installation
 
 default_action :create
 
-property :version, String, required: true
+property :version, String, default: '1.1.1c'
+property :download_directory, [String, nil], default: nil
+property :build_directory, [String, nil], default: nil
 property :install_directory, String, name_property: true
 property :owner, String, default: 'root'
 property :group, String, default: 'root'
+property :strict_security, [true, false], default: true
 
 action :create do
   create_openssl_install(@new_resource)

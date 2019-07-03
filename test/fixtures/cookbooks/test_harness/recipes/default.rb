469146ee86805d6dce9c231b5c126add703762d3
Caleb Severn <calsev@gmail.com>
2019-07-02 17:39:57 -0800

    Added a test recipe

--- test/fixtures/cookbooks/test_harness/recipes/default.rb
+++ test/fixtures/cookbooks/test_harness/recipes/default.rb
@@ -1,1 +1,23 @@
 # frozen_string_literal: true
+
+include_recipe 'openssl_install::default'
+
+openssl_installation 'All Defaults'
+
+directory '/usr/local/openssl-dl'
+
+directory '/usr/local/openssl-bld'
+
+directory '/usr/local/openssl'
+
+user 'bud'
+
+openssl_installation 'No Defaults' do
+  version '1.0.2s'
+  download_directory '/usr/local/openssl-dl'
+  build_directory '/usr/local/openssl-bld'
+  install_directory '/usr/local/openssl'
+  owner 'bud'
+  group 'bud'
+  strict_security false
+end

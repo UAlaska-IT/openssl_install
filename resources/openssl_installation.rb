28087ad4fd52f6d302d74ffb68d53c07d4745479
Caleb Severn <calsev@gmail.com>
2019-07-02 17:39:02 -0800

    Changed default install directory to nil

--- resources/openssl_installation.rb
+++ resources/openssl_installation.rb
@@ -8,7 +8,7 @@ default_action :create
 property :version, String, default: '1.1.1c'
 property :download_directory, [String, nil], default: nil
 property :build_directory, [String, nil], default: nil
-property :install_directory, String, name_property: true
+property :install_directory, [String, nil], default: nil
 property :owner, String, default: 'root'
 property :group, String, default: 'root'
 property :strict_security, [true, false], default: true

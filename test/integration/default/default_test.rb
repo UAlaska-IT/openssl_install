ac656ff795dea820818f36eda6f8800e2c144426
Caleb Severn <calsev@gmail.com>
2019-07-08 16:15:32 -0800

    Added tests for packages

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,1 +1,26 @@
 # frozen_string_literal: true
+
+node = json('/opt/chef/run_record/last_chef_run_node.json')['automatic']
+
+dev =
+  if node['platform_family'] == 'debian'
+    'dev'
+  else
+    'devel'
+  end
+
+describe package('gcc') do
+  it { should be_installed }
+end
+
+describe package('g++') do
+  it { should be_installed } if node['platform_family'] == 'debian'
+end
+
+describe package('gcc-c++') do
+  it { should be_installed } unless node['platform_family'] == 'debian'
+end
+
+describe package('make') do
+  it { should be_installed }
+end

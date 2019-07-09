3f5ed74e08b0b5d30f5dce85a4faf560e262862c
Caleb Severn <calsev@gmail.com>
2019-07-08 18:24:04 -0800

    Added tests for download and build directories

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -56,3 +56,53 @@ describe user('bud') do
   its('home') { should eq '/home/bud' }
   its('shell') { should eq '/bin/sh' }
 end
+
+# Begin white-box testing of resources
+
+describe file('/var/chef') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/var/chef/cache') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/var/chef/cache/openssl-1.1.1c.tar.gz') do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/openssl-dl/openssl-1.1.0k.tar.gz') do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'bud' }
+  it { should be_grouped_into 'bud' }
+end
+
+describe file('/var/chef/cache/openssl-1.1.1c') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o775 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/openssl-bld/openssl-1.1.0k') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o775 }
+  it { should be_owned_by 'bud' }
+  it { should be_grouped_into 'bud' }
+end
